cde750fb2c90dcf9432e00e46f5a97605e849620
Roger Howard <rogerhoward@gmail.com>
2016-07-28 22:05:56 -0700

    Refactor to use BytesIO

--- server.py
+++ server.py
@@ -6,30 +6,34 @@ Run by './server.py'
 Access properties as 'config.property'
 """
 
-import os, sys
-from flask import Flask, send_file, render_template, request
-from picamera import PiCamera
-from time import sleep
-import datetime, os
+import datetime
+from flask import Flask, send_file
+from picamera import PiCamera, PiCameraError
+from io import BytesIO
+
 
 app = Flask(__name__)
-
-
-
-tmp_dir = '/home/pi/images/'
 
 
 def stamped_name(base_filename, fmt='%Y-%m-%d-%H-%M-%S_{base}'):
     return datetime.datetime.now().strftime(fmt).format(base=base_filename)
 
+
 @app.route('/click')
 def click():
-    image_file = os.path.join(tmp_dir, stamped_name('image.jpg'))
+    new_file_name = stamped_name('image.jpg')
+    image_stream = BytesIO()
+
     camera = PiCamera()
+
     camera.resolution = (2592, 1944)
-    camera.capture(image_file)
+    camera.capture(image_stream, 'jpeg')
     camera.close()
-    return send_file(image_file, mimetype='image/jpeg')
+
+    return send_file(image_stream,
+                     mimetype='image/jpeg',
+                     attachment_filename=new_file_name)
+
 
 @app.route('/')
 def home():
